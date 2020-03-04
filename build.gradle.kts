0d36ff5d5fa44c1342b0ff679debeee8e49f03db
Andrew Sherepenko <andrew.sherepenko@gmail.com>
2020-03-04 19:15:53 +0200

    Update kotlin version [ci skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         classpath ("com.google.gms:google-services:4.3.3")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.0.0-beta02")
         classpath("com.google.firebase:perf-plugin:1.3.1")
-        classpath(kotlin("gradle-plugin", version = "1.3.61"))
+        classpath(kotlin("gradle-plugin", version = "1.3.70"))
     }
 }
 
