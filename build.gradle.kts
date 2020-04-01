d3679fb18bef7cd02acabd34ea92b54251d1136d
Andrew Sherepenko <andrew.sherepenko@gmail.com>
2020-04-01 22:43:54 +0300

    Update gradle and android plugin [si skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0-alpha04")
-        classpath("com.android.tools.build:gradle:3.6.1")
+        classpath("com.android.tools.build:gradle:3.6.2")
         classpath ("com.google.gms:google-services:4.3.3")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.0.0-beta03")
         classpath("com.google.firebase:perf-plugin:1.3.1")
