b0a820e3982de211c11b37890bd0852312c004d0
Andrew Sherepenko <andrew.sherepenko@gmail.com>
2020-04-16 13:51:20 +0300

    Code refactoring [ci skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         classpath ("com.google.gms:google-services:4.3.3")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.0.0-beta03")
         classpath("com.google.firebase:perf-plugin:1.3.1")
-        classpath(kotlin("gradle-plugin", version = "1.3.71"))
+        classpath(kotlin("gradle-plugin", version = "1.3.72"))
     }
 }
 
