fe4bd96fd5ab016cf16eb631b62cef09e4016984
Andrew Sherepenko <andrew.sherepenko@gmail.com>
2020-04-20 12:55:25 +0300

    Update deps [ci skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ buildscript {
     }
 
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0-alpha04")
-        classpath("com.android.tools.build:gradle:3.6.2")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.0-alpha05")
+        classpath("com.android.tools.build:gradle:3.6.3")
         classpath ("com.google.gms:google-services:4.3.3")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.0.0-beta03")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.0.0-beta04")
         classpath("com.google.firebase:perf-plugin:1.3.1")
         classpath(kotlin("gradle-plugin", version = "1.3.72"))
     }
