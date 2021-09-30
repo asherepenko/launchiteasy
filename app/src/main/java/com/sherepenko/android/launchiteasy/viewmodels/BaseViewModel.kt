c1da40778417c30fc0cbb684cedce2b2f5ef7484
Andrii Sherepenko <andrii.sherepenko@pray.com>
2021-09-30 22:05:27 +0300

    Code refactoring [ci skip]

--- app/src/main/java/com/sherepenko/android/launchiteasy/viewmodels/BaseViewModel.kt
+++ app/src/main/java/com/sherepenko/android/launchiteasy/viewmodels/BaseViewModel.kt
@@ -2,11 +2,10 @@ package com.sherepenko.android.launchiteasy.viewmodels
 
 import androidx.lifecycle.ViewModel
 import com.sherepenko.android.launchiteasy.repositories.BaseRepository
-import org.koin.core.component.KoinComponent
 
 abstract class BaseViewModel<R : BaseRepository>(
     private vararg val repositories: R
-) : ViewModel(), KoinComponent {
+) : ViewModel() {
 
     override fun onCleared() {
         super.onCleared()
