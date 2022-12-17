6ff5fb60e87b0d398eacb9ae2e22daa19ed5fa2a
Andrew Sherepenko <andrew.sherepenko@gmail.com>
2022-12-17 16:43:19 +0200

    Code refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.ksp) apply false
-    alias(libs.plugins.google.services) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.dependency.check) apply false
+    alias(libs.plugins.google.services) apply false
     alias(libs.plugins.androidx.navigation.safeargs) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.firebase.perf) apply false
